97878456b5670610a440923fe7b9054fe9ca4b89
0xsea <110372659+lhl200003@users.noreply.github.com>
2026-09-12 16:51:51 +0800

    Fix Arc Testnet chain hex

--- src/config/arc.ts
+++ src/config/arc.ts
@@ -1,7 +1,7 @@
 import { defineChain } from 'viem'
 
 export const ARC_CHAIN_ID = 5_042_002
-export const ARC_CHAIN_HEX = '0x4CF1D2'
+export const ARC_CHAIN_HEX = '0x4CEF52'
 export const ARC_RPC_URL = 'https://rpc.testnet.arc.network'
 export const ARC_EXPLORER_URL = 'https://testnet.arcscan.app'
 export const ARC_USDC_ADDRESS = '0x3600000000000000000000000000000000000000' as const
